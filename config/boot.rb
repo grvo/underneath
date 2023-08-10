7e86364d65cecdc86696f57189c342ee4dd2e0dc
furstamp <gyket.pentest.133971@protonmail.com>
2023-08-09 23:36:20 -0300

    boot de configuração para atribuição de gemfile

---
+++ config/boot.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+
+# configurar gemas listadas na gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
